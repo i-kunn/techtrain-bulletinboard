54f1a53d7c600769f019cee589b49b35466bec77
i-kunn <kohei100560@outlook.jp>
2025-05-07 15:17:27 +0900

    station1の作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-// src/App.jsx
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [threads, setThreads] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://railway.bulletinboard.techtrain.dev/threads')
+      .then((res) => {
+        setThreads(res.data);
+      })
+      .catch((err) => {
+        console.error('取得エラー:', err);
+      });
+  }, []);
+
   return (
     <div>
       <h1>掲示板アプリ</h1>
+      <ul>
+        {threads.map((thread) => (
+          <li key={thread.id}>{thread.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
