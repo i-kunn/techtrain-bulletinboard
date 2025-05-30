082e52a03566d866b71fa6e0e3c858ea701cd466
i-kunn <kohei100560@outlook.jp>
2025-05-30 18:03:54 +0900

    station2のデザインとコードの修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+// station3,4,5の修正コード
+// import './App.css';
+// import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+// import ThreadList from './pages/ThreadList';
+// import NewThread from './pages/NewThread';
+// import ThreadDetail from './pages/ThreadDetail';
+// // トップページ以外に表示させるようにしたコード
+// function NavigationLinks() {
+//   const location = useLocation();
+//   if (location.pathname === '/')
+//     return (
+//       <nav>
+//         <h2>新着情報</h2>
+//         <Link to="/threads/new">新規作成</Link>|
+//         <Link to="/threads/:thread_id">投稿一覧画面へ進む</Link>
+//       </nav>
+//     )
+//   return null;
+// }
+// function App() {
+//   return (
+  
+//     <Router>
+//       <div className="container">
+//         <h1>掲示板アプリ</h1>
+//         <NavigationLinks />
+//         <Routes>
+//           <Route path='/' element={<ThreadList />} />
+//           <Route path='/threads/new' element={<NewThread />} />
+//           <Route path="/threads/:thread_id" element={<ThreadDetail />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+// export default App;
+// station2について
+import './App.css';
 import ThreadList from './pages/ThreadList';
-import NewThread from './pages/NewThread';
-import ThreadDetail from './pages/ThreadDetail';
-import { use } from 'react';
-// トップページ以外に表示させるようにしたコード
-function NavigationLinks() {
-  const location = useLocation();
-  if (location.pathname === '/')
-    return (
-      <nav>
-        <Link to="/">スレッド一覧</Link>|
-        <Link to="/threads/new">新規作成</Link>|
-        <Link to="/threads/:thread_id">投稿一覧画面へ進む</Link>
-      </nav>
-    )
-  return null;
-}
+
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>掲示板アプリ</h1>
-        <NavigationLinks/>
-        <Routes>
-          <Route path='/' element={<ThreadList />} />
-          <Route path='/threads/new' element={<NewThread />} />
-          <Route path="/threads/:thread_id" element={<ThreadDetail />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="container">
+      <h1>掲示板アプリ</h1>
+      <h2>新着情報!!</h2>
+      <ThreadList />
+    </div>
   );
 }
+
 export default App;
--- src/pages/ThreadList.jsx
+++ src/pages/ThreadList.jsx
@@ -1,5 +1,36 @@
+// import { useEffect, useState } from 'react';   
+// import { Link } from 'react-router-dom'
+// function ThreadList() {
+//   const [threads, setThreads] = useState([]);
+
+//   useEffect(() => {
+//     fetch('https://railway.bulletinboard.techtrain.dev/threads')
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setThreads(data);
+//       })
+//       .catch((err) => {
+//         console.error('取得エラー:', err);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <ul>
+//         {threads.map((thread) => (
+//           <li key={thread.id}>
+//             <Link to={`/threads/${thread.id}`}>{thread.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+
+//     </div>
+//   );
+// }
+// export default ThreadList;
+// station2についてのコード
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'
+
 function ThreadList() {
   const [threads, setThreads] = useState([]);
 
@@ -19,15 +50,12 @@ function ThreadList() {
       <ul>
         {threads.map((thread) => (
           <li key={thread.id}>
-            <Link to={`/threads/${thread.id}`}>{thread.title}</Link>
+            {thread.title}
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
 
 export default ThreadList;
-
-
